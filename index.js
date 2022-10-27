95ddd5cbbb537913145c749ea1a461ed985438bc
Nickel-Dev <92646300+nickel-dev@users.noreply.github.com>
2022-10-27 17:10:46 +0200

    Version 2

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-function toggleCursor() {
+/*function toggleCursor() {
   const cursor = document.getElementById("cursor");
   if (cursor.innerHTML == "█") {
     cursor.innerHTML = "▯";
   } else if (cursor.innerHTML == "▯") {
     cursor.innerHTML = "█";
   }
+}*/
+
+addEventListener('resize', (event) => {});
+
+onresize = (event) => {
+  const width = window.innerWidth;
+  const navbar = document.getElementById("navbar");
+
+  if (width <= 1200)
+  {
+    navbar.style.visibility = "hidden";
+  }
+  else
+  {
+    navbar.style.visibility = "visible"; 
+  }
+};
+
+function changeTheme() {
+  return;
 }
 
-setInterval(toggleCursor, 500);
+//setInterval(toggleCursor, 500);
