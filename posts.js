cc0df6e249289cb40aa1e04bfeda47b0ffdf69f0
nickel-dev <nickel-dev@proton.me>
2024-09-07 21:44:19 +0200

    2024-09-07-Making_a_sustainable_Entity_system

--- posts.js
+++ posts.js
@@ -1,15 +1,22 @@
-const footer = '<div id="footnote"><hr><h3>Contacts:</h3><a href="https://github.com/nickel-dev">Github</a> | <a href="https://youtube.com/@cozyfog">Youtube</a> | <a href="mailto: nickel-dev@proton.me">nickel-dev@proton.me</a></div>'
-
 var url = window. location. pathname;
-var filename = url.substring(url.lastIndexOf('/')+1);
+var filename = url.substring(url.lastIndexOf('/') + 1);
+
+const footer = '<div id="footnote"><hr><h3>Contacts:</h3><a href="https://github.com/nickel-dev">Github</a> | <a href="https://youtube.com/@cozyfog">Youtube</a> | <a href="mailto: nickel-dev@proton.me">nickel-dev@proton.me</a></div>'
 
 function generate_header()
 {
-  let header = '';
+  let path = url.split('/');
+  path = path[path.length - 2];
+  console.log(path);
+  if (path == 'posts')
+    path += '/';
+  else
+    path = ''
+  let header = '<a href="https://github.com/nickel-dev/nickel-dev.github.io/blob/main/' + path + filename + '">source</a>';
   let name = filename.split('_').join(' ').replace('.html', '');
   const date = name.substring(0, name.lastIndexOf('-'));
   name = name.substring(name.lastIndexOf('-') + 1);
-  return '<title>' + date + ' - ' + name + '</title><i>' + date + '</i><h1>' + name + '</h1><a href="../"><< Back</a><hr></hr>'
+  return '<title>' + date + ' - ' + name + '</title><i>' + date + '</i><h1>' + name + '</h1><a href="../"><< Back</a><hr>' + header
 }
 
 function remove_new_line(name)
